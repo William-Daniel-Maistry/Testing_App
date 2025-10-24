305eee7ef93119ef52f53623053c917197e15c09
William-Daniel-Maistry <166234162+William-Daniel-Maistry@users.noreply.github.com>
2025-10-24 20:46:11 +0200

    Update reset.js
    changed supabase

--- reset.js
+++ reset.js
@@ -1,14 +1,12 @@
-import { createClient } from '@supabase/supabase-js'
-
 const supabaseUrl = 'https://yohbyoeuukvadrpgaxus.supabase.co';
 const supabaseKey = 'YOUR_ANON_KEY';
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseClient = createClient(supabaseUrl, supabaseKey);
 
 document.getElementById("updatePasswordBtn").addEventListener("click", async () => {
   const newPassword = document.getElementById("newPassword").value.trim();
   if (!newPassword) return alert("Enter a new password.");
 
-  const { data, error } = await supabase.auth.updateUser({
+  const { data, error } = await supabaseClient.auth.updateUser({
     password: newPassword
   });
 
@@ -20,4 +18,5 @@ document.getElementById("updatePasswordBtn").addEventListener("click", async () 
     window.location.href = "profile.html"; // back to login
   }
 });
+
 
